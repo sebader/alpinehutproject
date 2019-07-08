977a4622a3046e648f8ced66f96c662eb27d8514
Sebastian Bader <sebader@microsoft.com>
2019-07-08 14:20:43 +0200

    fixed map

--- WebUI/Controllers/MapController.cs
+++ WebUI/Controllers/MapController.cs
@@ -26,19 +26,19 @@ namespace WebUI.Cotrollers
 
         // GET: api/Map?llLat=1.2&llLon=89.23&urLat=2.3&urLon=78.3[&dateFilter=2019-11-16]
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<MapPlotHut>>> GetHuts([FromQuery] double llLat, [FromQuery] double llLon, [FromQuery] double urLat, [FromQuery] double urLon, [FromQuery] DateTime? dateFilter)
+        public async Task<ActionResult<IEnumerable<Hut>>> GetHuts([FromQuery] double llLat, [FromQuery] double llLon, [FromQuery] double urLat, [FromQuery] double urLon, [FromQuery] DateTime? dateFilter)
         {
             var huts = _context.Huts.Where(h => h.Latitude > llLat && h.Latitude < urLat && h.Longitude > llLon && h.Longitude < urLon);
             if (dateFilter != null)
             {
                 huts = huts.Where(h => h.Enabled == true && h.Availability.Any(a => a.Date == dateFilter && a.FreeRoom > 0));
             }           
-            var result = huts.Select(hut => new MapPlotHut { Id = hut.Id, Name = hut.Name, Details = hut.Region, Latitude = (double)hut.Latitude, Longitude = (double)hut.Longitude });   
+            //var result = huts.Select(hut => new MapPlotHut { Id = hut.Id, Name = hut.Name, Details = hut.Region, Latitude = (double)hut.Latitude, Longitude = (double)hut.Longitude });   
             if(huts.Count() > 0)
             {
                 _logger.LogInformation($"GetHuts returned {huts.Count()} huts");
             }
-            return await result.ToListAsync();
+            return await huts.ToListAsync();
         }
     }
 }
--- WebUI/Pages/Map.cshtml.cs
+++ WebUI/Pages/Map.cshtml.cs
@@ -22,25 +22,5 @@ namespace WebUI.Pages
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
         public DateTime? DateFilter { get; set; }
-
-
-        public IList<Hut> Huts { get; set; }
-
-        public async Task OnGetAsync(bool onlyEnabled = true, DateTime? DateFilter = null)
-        {
-            this.DateFilter = DateFilter?.Date;
-            /*
-            IQueryable<Hut> huts;
-            if (this.DateFilter != null)
-            {
-                huts = _context.Huts.Where(h => h.Enabled == true && h.Availability.Any(a => a.Date == this.DateFilter && a.FreeRoom > 0));
-            }
-            else
-            {
-                huts = _context.Huts.Where(h => h.Enabled == true || h.Enabled == onlyEnabled);
-            }
-            Huts = await huts.Include(h => h.Availability).ThenInclude(a => a.BedCategory).ToListAsync();
-            */
-        }
     }
 }

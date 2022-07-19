ff26eb284344177cbf9276888357810008f7b926
Sebastian <sebader@microsoft.com>
2022-07-19 17:16:06 +0200

    standalone api function

---
+++ website/api/WebsiteFunctions/GetHutByIdFunction.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using WebsiteBackendFunctions.Models;
+
+namespace WebsiteBackendFunctions
+{
+    public static class GetHutByIdFunction
+    {
+        [FunctionName(nameof(GetHut))]
+        public static IActionResult GetHut(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "hut/{hutid:int}")] HttpRequest req,
+            [Sql("SELECT * FROM [dbo].[Huts] WHERE id = @Id",
+            CommandType = CommandType.Text,
+            Parameters = "@Id={hutid}",
+            ConnectionStringSetting = "DatabaseConnectionString")] IEnumerable<Hut> result,
+            int hutId,
+            ILogger log)
+        {
+
+            if (result == null || result.Count() == 0)
+            {
+                log.LogInformation("Not hut found for id {hutid}", hutId);
+                return new NotFoundResult();
+            }
+
+            var hut = result.FirstOrDefault();
+            log.LogInformation("Retrieved hut {hutName} for id {hutid}", hut?.Name, hutId);
+            return new OkObjectResult(hut);
+        }
+    }
+}
--- website/public/config.js
+++ website/public/config.js
@@ -1,5 +1,7 @@
 // Use these values for local development.
 // This whole file will be replaced during deployment, with environment appropriate values.
-window.API_URL = "http://localhost:7199/api"; // without the trailing "/"
+//window.API_URL = "http://localhost:7199/api"; // without the trailing "/"
+
+window.API_URL = "/api"; // without the trailing "/"
 window.APPLICATIONINSIGHTS_CONNECTION_STRING = "InstrumentationKey=84f8aded-df81-4ade-a50b-0a578a30eca9;IngestionEndpoint=https://westeurope-3.in.applicationinsights.azure.com/;LiveEndpoint=https://westeurope.livediagnostics.monitor.azure.com/";
 window.VERSION_LABEL = "local"

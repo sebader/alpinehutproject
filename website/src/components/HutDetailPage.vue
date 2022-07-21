d10f41027b7d64a9d0c5bd01910755bc6f1b60d6
Sebastian <sebader@microsoft.com>
2022-07-21 14:00:39 +0200

    adding map to hut details page

--- website/src/components/HutDetailPage.vue
+++ website/src/components/HutDetailPage.vue
@@ -4,8 +4,8 @@
       <p v-show="loading">Loading...</p>
 
       <div v-if="!loading && hut != null">
-         <div>
-            <div>
+         <div class="row">
+            <div class="col-sm">
                <h1>{{ hut.name }}</h1>
                <table>
                   <tr>
@@ -30,33 +30,55 @@
                   </tr>
                   <tr>
                      <td>Coordinates</td>
-                     <td><router-link v-if="hut.latitude != null && hut.longitude != null" :to="{ name: 'mapPage', query: { hutId: hut.id } }" >{{ hut.latitude?.toLocaleString() }}/{{ hut.longitude?.toLocaleString() }}</router-link></td>
+                     <td>
+                        <router-link v-if="hut.latitude != null && hut.longitude != null"
+                           :to="{ name: 'mapPage', query: { hutId: hut.id } }">{{ hut.latitude }}/{{ hut.longitude }}
+                        </router-link>
+                     </td>
                   </tr>
                </table>
             </div>
+            <div class="col-sm">
+               <div style="height: 35vh;">
+                  <l-map ref="map" v-model:zoom="zoom" :center="mapCenter" :minZoom="6" :maxZoom="17">
+                     <l-control-layers position="topright"></l-control-layers>
+                     <l-tile-layer v-for="tileProvider in tileProviders" :key="tileProvider.name"
+                        :name="tileProvider.name" :visible="tileProvider.visible" :url="tileProvider.url"
+                        :attribution="tileProvider.attribution" layer-type="base" />
+                     <l-marker ref="markerItems" :name="hut.name" :lat-lng="[hut.latitude, hut.longitude]"
+                        :icon="markerIcon">
+                        <l-tooltip>
+                           <b>{{ hut.name }}</b>
+                        </l-tooltip>
+                     </l-marker>
+                  </l-map>
+               </div>
+            </div>
          </div>
-         <br />
-         <div>
-            <table>
-               <thead>
-                  <tr>
-                     <th>Date</th>
-                     <th>Beds</th>
-                     <th>Type of accommodation</th>
-                  </tr>
-               </thead>
-               <tbody>
-                  <template v-for="av in hut.availability">
-                     <tr v-for="(roomAv, iSub) in av.roomAvailabilities" :key="roomAv.bedCategory">
-                        <td v-if="iSub === 0" :rowspan="av.roomAvailabilities.length">{{ new
-                              Date(av.date).toDateString("dddd, dd.MM.yyyy")
-                        }}</td>
-                        <td>{{ roomAv.freeBeds }} / {{ roomAv.totalBeds }}</td>
-                        <td>{{ roomAv.bedCategory }}</td>
+         <div class="row">
+            <br />
+            <div class="col">
+               <table>
+                  <thead>
+                     <tr>
+                        <th>Date</th>
+                        <th>Beds</th>
+                        <th>Type of accommodation</th>
                      </tr>
-                  </template>
-               </tbody>
-            </table>
+                  </thead>
+                  <tbody>
+                     <template v-for="av in hut.availability">
+                        <tr v-for="(roomAv, iSub) in av.roomAvailabilities" :key="roomAv.bedCategory">
+                           <td v-if="iSub === 0" :rowspan="av.roomAvailabilities.length">{{ new
+                                 Date(av.date).toDateString("dddd, dd.MM.yyyy")
+                           }}</td>
+                           <td>{{ roomAv.freeBeds }} / {{ roomAv.totalBeds }}</td>
+                           <td>{{ roomAv.bedCategory }}</td>
+                        </tr>
+                     </template>
+                  </tbody>
+               </table>
+            </div>
          </div>
       </div>
    </section>
@@ -77,10 +99,57 @@
 import { Constants } from '../utils';
 import { EventBus } from "../main"
 
+import L from 'leaflet';
+import {
+   LMap,
+   LIcon,
+   LTileLayer,
+   LMarker,
+   LControlLayers,
+   LTooltip,
+} from "@vue-leaflet/vue-leaflet";
+import "leaflet/dist/leaflet.css";
+
 export default {
+   components: {
+      LMap,
+      LIcon,
+      LTileLayer,
+      LMarker,
+      LControlLayers,
+      LTooltip,
+   },
    data: function () {
       return {
-         loading: false
+         hut: null,
+         loading: false,
+         mapCenter: null,
+         zoom: 10,
+         tileProviders: [
+            {
+               name: 'OpenStreetMap',
+               visible: true,
+               minZoom: 6,
+               attribution:
+                  '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+               url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+            },
+            {
+               name: 'OpenTopoMap',
+               visible: false,
+               url: 'https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
+               minZoom: 6,
+               maxZoom: 17,
+               attribution:
+                  'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)',
+            }
+         ],
+         markerIcon: L.icon({
+            iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-blue.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34]
+         })
       }
    },
    computed: {
@@ -102,6 +171,7 @@ export default {
          else {
             this.hut = await this.$HutService.getHutByIdAsync(hutId);
             this.hut.availability = await this.$AvailabilityService.getAvailabilityForHut(this.hut.id);
+            this.mapCenter = [this.hut.latitude, this.hut.longitude];
          }
       }
       catch (e) {

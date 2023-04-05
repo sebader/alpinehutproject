1d91ad0f990ec53859281a501ae2612446e6a9fb
Sebastian <mail@silenced.eu>
2023-04-05 13:14:38 +0200

    test db connection

--- website/src/services/hut-service.js
+++ website/src/services/hut-service.js
@@ -2,6 +2,7 @@ import { processErrorResponseAsync } from "../utils"
 import { Constants } from "../utils";
 
 const API_ENDPOINT = window.API_URL;
+const DATA_API_ENDPOINT = window.DATA_API_URL;
 
 export default class HutService {
 
@@ -9,7 +10,7 @@ export default class HutService {
 
    async listHutsAsync() {
       if (this.huts == null) {
-         var res = await fetch(`${API_ENDPOINT}/hut`);
+         var res = await fetch(`${DATA_API_ENDPOINT}/Hut`);
          if (res.ok) {
             this.huts = res.json();
          }
@@ -29,7 +30,7 @@ export default class HutService {
 
    async getHutByIdAsync(hutId) {
 
-      var res = await fetch(`${API_ENDPOINT}/hut/${hutId}`);
+      var res = await fetch(`${DATA_API_ENDPOINT}/Hut/Id/${hutId}`);
 
       if (res.ok) {
          return await res.json();

4949cf8fd082f1eb6751ae4a1d116d1e32903e8f
Sebastian Bader <sebader@microsoft.com>
2025-04-14 22:21:22 +0200

    map full screen

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,36 +1,39 @@
 <template>
-  <div>
-    <div class="header">
+  <div :class="{ 'map-layout': $route.name === 'mapPage' }">
+    <div class="header" :class="{ 'map-header': $route.name === 'mapPage' }">
       <h4>{{ $t("message.siteTitle") }}</h4>
-      <router-link :to="{ name: 'mapPage' }">{{ $t("message.map") }}</router-link> |
-      <router-link :to="{ name: 'hutListPage' }">{{ $t("message.list") }}</router-link> |
-      <router-link :to="{ name: 'infoPage' }">{{ $t("message.info") }}</router-link> |
-      <template v-if="isAdmin">
-        <a href="/logout">{{ $t("message.logout") }}</a>
-      </template>
-      <template v-else>
-        <a href="/login">{{ $t("message.login") }}</a>
-      </template>
+      <template v-if="$route.name !== 'mapPage'">
+        <router-link :to="{ name: 'mapPage' }">{{ $t("message.map") }}</router-link> |
+        <router-link :to="{ name: 'hutListPage' }">{{ $t("message.list") }}</router-link> |
+        <router-link :to="{ name: 'infoPage' }">{{ $t("message.info") }}</router-link> |
+        
+        <template v-if="isAdmin">
+          <a href="/logout">{{ $t("message.logout") }}</a>
+        </template>
+        <template v-else>
+          <a href="/login">{{ $t("message.login") }}</a>
+        </template>
 
-      <span style="float:right;">
-        <span v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`">
-          <router-link
-            :to="{ name: $route.name, params: $route.params, query: $route.query, hash: $route.hash, replace: true }"
-            :class="{ 'disabled-link': $i18n.locale === locale }" v-if="$i18n.locale !== locale"
-            @click.native="$i18n.locale = locale">{{ $t("message.locale" + locale) }}</router-link>
-          <span v-else>{{ $t("message.locale" + locale) }}</span>
-          <span v-if="locale !== $i18n.availableLocales[$i18n.availableLocales.length - 1]"> | </span>
+        <span style="float:right;">
+          <span v-for="locale in $i18n.availableLocales" :key="`locale-${locale}`">
+            <router-link
+              :to="{ name: $route.name, params: $route.params, query: $route.query, hash: $route.hash, replace: true }"
+              :class="{ 'disabled-link': $i18n.locale === locale }" v-if="$i18n.locale !== locale"
+              @click.native="$i18n.locale = locale">{{ $t("message.locale" + locale) }}</router-link>
+            <span v-else>{{ $t("message.locale" + locale) }}</span>
+            <span v-if="locale !== $i18n.availableLocales[$i18n.availableLocales.length - 1]"> | </span>
+          </span>
         </span>
-      </span>
-      <hr />
+      </template>
+      <hr v-if="$route.name !== 'mapPage'" />
 
-      <SystemMessage />
+      <SystemMessage v-if="$route.name !== 'mapPage'" />
     </div>
     <router-view :key="$route.fullPath" :isAuthenticated="isAdmin"></router-view>
 
-    <hr />
+    <hr v-if="$route.name !== 'mapPage'" />
 
-    <footer>{{ $t("message.footerText") }}. Commit version: {{ versionLabel }}
+    <footer v-if="$route.name !== 'mapPage'">{{ $t("message.footerText") }}. Commit version: {{ versionLabel }}
     </footer>
   </div>
 </template>
@@ -67,6 +70,28 @@ export default {
 </script>
 
 <style>
+.map-layout {
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+}
+
+.map-layout .header {
+  position: relative;
+  z-index: 2500;
+  background: #fff;
+  padding: 5px 15px;
+  margin: 0;
+  border-bottom: 1px solid #eee;
+  box-shadow: 0 1px 4px rgba(0,0,0,0.1);
+}
+
+.header {
+  background: #fff;
+  padding: 10px;
+  margin-bottom: 20px;
+}
+
 footer {
   font-style: italic;
   font-size: 0.8em;
